ea0a6f9d6f80b026638faa30bfef75745f793c4e
Aaditya-Chunekar <aadityachunekar@gmail.com>
2026-01-29 13:02:35 +0530

    [version] 1.0.1

--- cf.sh
+++ cf.sh
@@ -6,11 +6,11 @@ n=$#
 # always quote - "*$anything*"
 for arg in "$@"
 do
-	res=$(curl -s "https://codeforces.com/api/user.rating?handle=$arg")
+	res=$(curl -s "https://codeforces.com/api/user.info?handles=$arg")
 	sts=$(echo "$res" | cut -d ":" -f 2 | cut -d "," -f 1)
 	if [ "$sts" =  '"OK"' ]; then
 		printf "\e[4m$arg\e[0m exists.\n"
-		printf "$(echo $res | awk -F':' '{print $NF}' | cut -d '}' -f 1) is the latest rating.\n" 
+		# printf "$(echo $res | awk -F':' '{print $NF}' | cut -d '}' -f 1) is the latest rating.\n" 
 	else
 		printf "\e[4m$arg\e[0m doesn't exist.\n"
 	fi
